90b988dedaa9f660324d374f147a0bb60cf941a0
achmadhadikurnia <4408971+achmadhadikurnia@users.noreply.github.com>
2025-10-21 04:07:08 +0000

    Apply automatic changes

--- database/seeders/ShieldSeeder.php
+++ database/seeders/ShieldSeeder.php
@@ -3,8 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 use Spatie\Permission\PermissionRegistrar;
 
 class ShieldSeeder extends Seeder
@@ -99,7 +99,7 @@ class ShieldSeeder extends Seeder
 
                 if (! blank($rolePlusPermission['permissions'])) {
                     $permissionModels = collect($rolePlusPermission['permissions'])
-                        ->map(fn($permission) => Permission::firstOrCreate([
+                        ->map(fn ($permission) => Permission::firstOrCreate([
                             'name' => $permission,
                             'guard_name' => $rolePlusPermission['guard_name'],
                         ]))
